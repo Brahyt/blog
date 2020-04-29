574b051097a10a0bb09c65326f9655dcb1d1bf19
Daniel Bright <brahyt@gmail.com>
2020-04-28 18:35:23 -0700

    added destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,12 +7,22 @@ class ArticlesController < ApplicationController
     render json: Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+    render json: @article
+  end
 
   def create
     params.permit!
     @article = Article.new(params.require(:articles))
     @article.save
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+  end
+
   private
   def articles_param
     params.require(:articles).permit(:title, :body)
